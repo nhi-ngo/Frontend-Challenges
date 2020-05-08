889308d20ef0502a5840518d2bcf61ed1e2dd67f
nhi-ngo <nhingo.y@gmail.com>
2020-05-07 22:51:54 -0400

    adding demo page for all challenges

---
+++ script.js
@@ -0,0 +1,55 @@
+const projects = [
+  {
+    name: 'huddle-landing-page',
+  },
+  {
+    name: 'four-card-feature',
+  },
+  {
+    name: 'fylo-landing-page',
+  },
+  {
+    name: 'base-apparel',
+  },
+  {
+    name: 'signup-form',
+  },
+  {
+    name: 'price-grid',
+  },
+  {
+    name: 'ping-landing-page',
+  },
+
+  {
+    name: 'insure-landing-page',
+  },
+  {
+    name: 'pricing-toggle',
+  },
+  {
+    name: 'clipboard-landing-page',
+  },
+];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name }, i) => {
+  const listItem = document.createElement('li');
+
+  listItem.innerHTML = `
+		<a href = "./${name}/index.html" >
+			<img src="./${name}/design/desktop-design.jpg" alt="${name}" />
+    </a>
+    <p>${i + 1}. ${formatName(name)}</p>
+
+	`;
+  list.appendChild(listItem);
+});
+
+function formatName(name) {
+  return name
+    .split('-')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+}
