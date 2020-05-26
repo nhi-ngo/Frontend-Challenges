663f2cb075ab3a81ce0fc5c64935adc70cec7b3a
nhi-ngo <nhingo.y@gmail.com>
2020-05-26 15:53:24 -0400

    job-listings-react completed

--- job-listings-react/src/components/App.js
+++ job-listings-react/src/components/App.js
@@ -4,17 +4,79 @@ import JobBoard from './JobBoard';
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => setJobs(data), []);
 
-  console.log(jobs);
+  const filterFunc = ({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    if (tools) {
+      tags.push(...tools);
+    }
+
+    return filters.every(filter => tags.includes(filter));
+  };
+
+  const onTagsClick = tag => {
+    // avoid re-adding the tag
+    if (filters.includes(tag)) return;
+
+    setFilters([...filters, tag]);
+  };
+
+  const onFilterClick = passedFilter => {
+    setFilters(filters.filter(f => f !== passedFilter));
+  };
+
+  const onClearFilters = () => {
+    setFilters([]);
+  };
+
+  const filteredJobs = jobs.filter(filterFunc);
 
   return (
-    <div>
-      <header className="mb-20 -mt-16">
-        <img className="md:min-w-full" src="images/bg-header-desktop.svg" alt="bg-img" />
+    <div className="App">
+      <header header className="-mt-16">
+        <img className="w-full" src="images/bg-header-desktop.svg" alt="bg-img" />
       </header>
-      {jobs.length === 0 ? <p>Jobs are fetching...</p> : jobs.map(job => <JobBoard job={job} key={job.id} />)}
+
+      <div className="container m-auto">
+        {/* Show the span if there is filters */}
+        {filters.length > 0 && (
+          <div className="flex justify-between bg-white shadow-lg border-teal-700 rounded p-6 -mt-10 mx-10 relative z-10">
+            <div>
+              {filters.map(filter => (
+                <span
+                  onClick={() => onFilterClick(filter)}
+                  className="cursor-pointer font-bold text-md p-2 rounded mr-3"
+                >
+                  {filter}
+                  <span className="text-teal-700 ml-1 mb"> &#10008; </span>
+                </span>
+              ))}
+            </div>
+
+            <button onClick={onClearFilters} className="cursor-pointer bg-transparent text-clear font-bold">
+              Clear
+            </button>
+          </div>
+        )}
+
+        {jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+          filteredJobs.map(job => <JobBoard job={job} key={job.id} onTagsClick={onTagsClick} />)
+        )}
+      </div>
     </div>
   );
 }
@@ -28,5 +90,5 @@ export default App;
 // 4. Pass down the state to JBC ✅
 // 5a. Style it desktop ✅
 // 5b. Style it mobile ✅
-// 6. Filter component
-// 7. Filter the data
+// 6. Filter component ✅
+// 7. Filter the data ✅
--- job-listings-react/src/components/JobBoard.js
+++ job-listings-react/src/components/JobBoard.js
@@ -29,14 +29,14 @@ const JobBoard = props => {
 
   return (
     <div
-      className={`flex flex-col bg-white shadow-lg my-16 mx-10 p-6 rounded-md ${featured &&
-        'border-l-4 border-teal-600'} md:flex-row md:items-center md:-mb-8 `}
+      className={`flex flex-col bg-white shadow-lg my-16 mx-10 p-6 rounded-lg ${featured &&
+        'border-l-4 border-teal-600'} lg:flex-row lg:items-center lg:-mb-8 `}
     >
       <div>
-        <img className="w-16 h-16 -mt-16 md:mt-0 md:w-20 md:h-20" src={logo} alt={company} />
+        <img className="w-16 h-16 -mt-16 lg:mt-0 lg:w-20 lg:h-20" src={logo} alt={company} />
       </div>
 
-      <div className="flex flex-col justify-between mt-4 md:ml-5">
+      <div className="flex flex-col justify-between mt-4 lg:ml-5">
         <h3 className="font-bold">
           {company}
           {isNew && <span className="text-white text-xs pt-2 px-3 py-1 rounded-full uppercase m-2">New!</span>}
@@ -48,8 +48,14 @@ const JobBoard = props => {
         </p>
       </div>
 
-      <div className="flex flex-wrap items-center justify-start mt-4 -mb-4 pt-5 border-t border-gray-400 md:ml-auto md:border-t-0 md:mt-0 md:pt-0">
-        {tags ? tags.map(tag => <span className="font-bold text-md mr-4 mb-4 p-2 rounded">{tag}</span>) : ''}
+      <div className="flex flex-wrap items-center justify-start mt-4 -mb-4 pt-5 border-t border-gray-400 lg:ml-auto lg:border-t-0 lg:mt-0 lg:pt-0">
+        {tags
+          ? tags.map(tag => (
+              <span onClick={() => props.onTagsClick(tag)} className="cursor-pointer font-bold text-lg mr-4 mb-4 p-2 rounded">
+                {tag}
+              </span>
+            ))
+          : ''}
       </div>
     </div>
   );
@@ -57,18 +63,3 @@ const JobBoard = props => {
 
 export default JobBoard;
 
-// {
-//   "id": 1,
-//   "company": "Photosnap",
-//   "logo": "./images/photosnap.svg",
-//   "isNew": true,
-//   "featured": true,
-//   "position": "Senior Frontend Developer",
-//   "role": "Frontend",
-//   "level": "Senior",
-//   "postedAt": "1d ago",
-//   "contract": "Full Time",
-//   "location": "USA Only",
-//   "languages": ["HTML", "CSS", "JavaScript"],
-//   "tools": []
-// }
