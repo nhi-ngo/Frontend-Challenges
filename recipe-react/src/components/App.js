e122f19233a39dd2766377a91084749dcde6b117
nhi-ngo <nhingo.y@gmail.com>
2020-05-28 19:51:42 -0400

    working on RandonMeals component

--- recipe-react/src/components/App.js
+++ recipe-react/src/components/App.js
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import RandomMeals from './RandomMeals';
 
-const App = () => <div>Recipe</div>;
+const App = () => (
+  <div className="App">
+    <RandomMeals />
+  </div>
+);
 
 export default App;
+
+// TODO
+// 1.Search for meals
+// 2.Display the results
+// 3.Display Random/Featured meals
+// Bonus: Bookmark favorite recipe
