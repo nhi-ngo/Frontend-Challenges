0b61d36912bc69046a2d712d03be4d6dcf4b86fc
nhi-ngo <nhingo.y@gmail.com>
2020-07-30 14:42:57 -0400

    animation

--- easybank-landing-page/index.js
+++ easybank-landing-page/index.js
@@ -2,8 +2,32 @@ const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 const overlay = document.querySelector('.overlay');
 
-hamburger.addEventListener('click', () => {
+function openNav() {
   hamburger.classList.toggle('show');
   menu.classList.toggle('show');
   overlay.classList.toggle('show');
-});
+}
+
+function closeNav() {
+  hamburger.classList.remove('show');
+  menu.classList.remove('show');
+  overlay.classList.remove('show');
+}
+
+function handleSmoothScroll() {
+  const anchors = document.querySelectorAll('#menu a');
+
+  anchors.forEach(anchor => {
+    anchor.addEventListener('click', e => {
+      const sectionContent = document.querySelector(e.target.hash);
+
+      e.preventDefault();
+      sectionContent.scrollIntoView({ behavior: 'smooth' });
+
+      closeNav();
+    });
+  });
+}
+
+hamburger.addEventListener('click', openNav);
+window.addEventListener('load', handleSmoothScroll);
